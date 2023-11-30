9b6d4bb104d8f9431edec17ba919408702267ea7
JovanSV64 <vuckovic.sv64.2021@uns.ac.rs>
2023-11-30 12:53:12 +0100

    accommodatino details
    without reservation component

--- BookingAppTeam18/src/app/app-routing.module.ts
+++ BookingAppTeam18/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./layout/home/home.component";
 import {AccommodationsComponent} from "./accommodation/accommodations/accommodations.component";
+import {DetailsComponent} from "./accommodation/details/details.component";
 import {CreateAccommodationComponent} from "./accommodation/create-accommodation/create-accommodation.component";
 import {LoginComponent} from "./user/login/login.component";
 import {RegisterComponent} from "./user/register/register.component";
@@ -11,6 +12,7 @@ import {UserInfoComponent} from "./profile/user-info/user-info.component";
 const routes: Routes = [
   {component: HomeComponent, path:"home"},
   {component: AccommodationsComponent, path:""},
+  {component: DetailsComponent, path:"details"},
   {component: CreateAccommodationComponent, path:"create-accommodation"},
   {component: LoginComponent, path:"log-in"},
   {component: RegisterComponent, path:"register"},
