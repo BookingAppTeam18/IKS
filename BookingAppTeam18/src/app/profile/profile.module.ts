58a4b0e21f42480a8b1936d2bd53ac9f00a74161
ZivanicBojan <zivanic.sv61.2021@uns.ac.rs>
2023-12-12 20:38:52 +0100

    Added edit user component

--- BookingAppTeam18/src/app/profile/profile.module.ts
+++ BookingAppTeam18/src/app/profile/profile.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserInfoComponent } from './user-info/user-info.component';
 import { CommentsComponent } from './comments/comments.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 
 
 @NgModule({
   declarations: [
     UserInfoComponent,
-    CommentsComponent
+    CommentsComponent,
+    EditUserComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     UserInfoComponent,
-    CommentsComponent
+    CommentsComponent,
+    EditUserComponent
   ]
 })
 export class ProfileModule { }
