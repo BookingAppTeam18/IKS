73e57f3497ae8bb3bc1c022a279b6e56a5900508
JovanSV64 <vuckovic.sv64.2021@uns.ac.rs>
2023-12-20 23:10:29 +0100

    benefit drawing

--- BookingAppTeam18/src/app/accommodation/details/details.component.ts
+++ BookingAppTeam18/src/app/accommodation/details/details.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {AccommodationService} from "../service/accommodation.service";
 import {AccommodationDetails} from "./model/accommodationDetails";
+import {Benefit} from "../accommodations/model/benefit";
+
+
+export class BenefitIcon{
+  benefit:Benefit;
+  icon:string;
+  constructor(benefit: Benefit, icon: string) {
+    this.benefit = benefit;
+    this.icon = icon;
+
+  }
+
+}
 
 @Component({
   selector: 'app-details',
@@ -10,6 +23,17 @@ import {AccommodationDetails} from "./model/accommodationDetails";
 })
 export class DetailsComponent {
   accommodationDetailsDTO: AccommodationDetails;
+  benefitIcons: BenefitIcon[]=[
+    new BenefitIcon(Benefit.WIFI,"wifi"),
+    new BenefitIcon(Benefit.FREE_PARKING,"local_parking"),
+    new BenefitIcon(Benefit.TV,"tv"),
+    new BenefitIcon(Benefit.AIR_CONDITIONER,"air conditioner"),
+    new BenefitIcon(Benefit.BACKYARD,"outdoor_grill"),
+    new BenefitIcon(Benefit.BALCONY,"balcony"),
+    new BenefitIcon(Benefit.ELEVATOR,"elevator"),
+    new BenefitIcon(Benefit.KITCHEN,"kitchen"),
+    new BenefitIcon(Benefit.PET_FRIENDLY,"pet"),
+  ];
 
   constructor(private route: ActivatedRoute, private accommodationService: AccommodationService) {
   }
@@ -18,8 +42,21 @@ export class DetailsComponent {
     this.route.params.subscribe((params) => {
       const id = +params['accommodationId']
       this.accommodationService.getAccommodationDetails(id).subscribe({
-        next: (data: AccommodationDetails) => { this.accommodationDetailsDTO = data }
+        next: (data: AccommodationDetails) => { this.accommodationDetailsDTO = data
+        console.log(data.accommodationDTO.benefits)}
       })
     })
+  }
+
+  getBenefitIcon(benefit: Benefit):string {
+
+    const benefitEnum = typeof benefit === 'string' ? Benefit[benefit] : benefit;
+
+    for (let benefitIcon of this.benefitIcons){
+        if(benefitIcon.benefit === benefitEnum){
+          return benefitIcon.icon
+        }
+    }
+    return "none";
   }
 }
